842ccec0b19b4e9ed49f72693be6516d7187ba59
John McKirdy <57973241+jmckirdy@users.noreply.github.com>
2024-09-26 22:23:44 -0700

    updates

---
+++ something.js
@@ -0,0 +1,17 @@
+let listOfThings = []
+let phrase = "the quick brown fox jumped over the lazy dog"
+
+for (let i=0;i<2;i++) {
+    console.log("HellO O11O01" + i + " " + phrase)
+}
+
+// while loop
+// 
+i = 0
+
+while(i < 3) {
+    listOfThings.push[1]
+    console.log("hell yeah..." + listOfThings)
+    i = i +1
+
+}
